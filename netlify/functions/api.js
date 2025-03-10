526e98f67ffdde17750a11506d2ab1c36d5913fb
davidweji <130021498+davidweji@users.noreply.github.com>
2025-03-09 21:48:02 -0300

    testing errors

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -21,9 +21,15 @@ if(!diet && !intolerances && !includeIngredients && !excludeIngredients) {
 
 const urlElements = new URLSearchParams({apiKey: API_KEY});
 if(diet) {urlElements.append("diet", diet);}
+console.log("Diet: ",diet);
 if(intolerances) {urlElements.append("intolerances",intolerances);}
+console.log("Intolerances:",intolerances);
 if(includeIngredients) {urlElements.append("includeIngredients",includeIngredients);}
+console.log("include Ingredients:",includeIngredients);
 if(excludeIngredients) {urlElements.append("excludeIngredients",excludeIngredients);}
+console.log("exclude Ingredients:",excludeIngredients);
+const Furl = `https://api.spoonacular.com/recipes/complexSearch?${urlElements.toString()}`;
+console.log("final url:",Furl);
 
 try {
     const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?${urlElements.toString()}`);
